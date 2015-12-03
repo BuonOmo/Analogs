50a1f0db3a586439fada280e0a6a73e756167bcc
BuonOmo <buonomo.ulysse@gmail.com>
2015-12-03 16:36:10 +0100

    avancée dans la lecture de la date (Read.cpp)

--- src/temporary/test_read.cpp
+++ src/temporary/test_read.cpp
@@ -1,36 +1,10 @@
-/*************************************************************************
-                      Read  -  classe de lecture de log
-                             -------------------
-    début                : 1 dec. 2015
-    copyright            : (C) 2015 par Ulysse et Adrien
-*************************************************************************/
-
-///---------- Réalisation de la classe <Read> (fichier Read.cpp) --
-
-///---------------------------------------------------------------- INCLUDE
-
-///-------------------------------------------------------- Include système
 using namespace std;
 #include <iostream>
 #include <string>
 #include <fstream>
 
-///------------------------------------------------------ Include personnel
-#include "Read.h"
 
-///------------------------------------------------------------- Constantes
-
-///---------------------------------------------------- Variables de classe
-
-///----------------------------------------------------------- Types privés
-
-
-///----------------------------------------------------------------- PUBLIC
-///-------------------------------------------------------- Fonctions amies
-
-///----------------------------------------------------- Méthodes publiques
-
-bool Read::hasNextLog ( )
+bool hasNextLog ( )
 {
 #ifdef MAP
     cout << "Appel à la methode Read::hasNextLog" << endl;
@@ -55,7 +29,7 @@ bool Read::hasNextLog ( )
 	return false;
 } //----- Fin de hasNextLog
 
-Log Read::readNextLog ( )
+Log readNextLog ( )
 {
 #ifdef MAP
     cout << "Appel à la methode Read::readNextLog" << endl;
@@ -112,7 +86,7 @@ Log Read::readNextLog ( )
             break;
         case "Dec" : date.month ( 12 );
             break;
-        default : date.month ( -1 );
+        default    : date.month ( -1 );
             break;
     }
     streamDate >> date.hour
@@ -123,68 +97,21 @@ Log Read::readNextLog ( )
     return new Log(date, root, target);
 } //----- Fin de readNextLog
 
-///------------------------------------------------- Surcharge d'opérateurs
-Read & Read::operator = ( const Read & unRead )
-// Algorithme :
-//
+int main(int argc, char* argv[])
 {
-	this.file = unRead.file;
-} //----- Fin de operator =
-
-
-///-------------------------------------------- Constructeurs - destructeur
-Read::Read ( const Read & unRead )
-// Algorithme :
-//
-{
-#ifdef MAP
-    cout << "Appel au constructeur de copie de <Read>" << endl;
-#endif
-
-    if (&this != &unRead)
-    {
-    	this = unRead;
-    }
-} //----- Fin de Read (constructeur de copie)
-
-
-Read::Read ( string aFile )
-// Algorithme :
-//
-{
-#ifdef MAP
-    cout << "Appel au constructeur de <Read>" << endl;
-#endif
-
-    if (aFile.rfind(".log\0") != string::npos)
-    {
-    	cerr << "Le fichier selectionné n’est pas un fichier .log"<< endl;
-    	return 1;
-    }
-
-    file(aFile, ios::in);
-
-    if (!file)
-    {
-		cerr << "Impossible d'ouvrir le fichier " << aFile << '.' <<endl;
-		return 1;
-	}
-
-} //----- Fin de Read
-
-
-Read::~Read ( )
-// Algorithme :
-//
-{
-#ifdef MAP
-    cout << "Appel au destructeur de <Read>" << endl;
-#endif
-} //----- Fin de ~Read
-
-
-///------------------------------------------------------------------ PRIVE
-
-///----------------------------------------------------- Méthodes protégées
-
-///------------------------------------------------------- Méthodes privées
+        ifstream file ("/home/ulysse/Eclipse/TP3/anonyme.log"/*argv[1]*/, ios::in);  // on ouvre en lecture
+ 
+        if (file)  // si l'ouverture a fonctionné
+        {
+                string toPrint[2];
+                while (file)
+                {
+                    readNextLine(file);
+                }
+                file.close();
+        }
+        else
+                cerr << "Impossible d'ouvrir le fichier "<< argv[1] << endl;
+ 
+        return 0;
+}
