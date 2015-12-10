c256128dce232ef5799cab443d053aa923e06960
BuonOmo <buonomo.ulysse@gmail.com>
2015-12-10 21:19:11 +0100

    Implementation de la classe Date

---
+++ src/Date.cpp
@@ -0,0 +1,112 @@
+/*************************************************************************
+                           Date  -  description
+                             -------------------
+    début                : 10/12/2015
+    copyright            : (C) 2015 par ulysse
+*************************************************************************/
+
+//------------------- Réalisation de la classe <Date> (fichier Date.cpp) --
+
+//----------------------------------------------------------------- INCLUDE
+
+//--------------------------------------------------------- Include systéme
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------- Include personnel
+#include "Date.h"
+
+//-------------------------------------------------------------- Constantes
+
+//----------------------------------------------------- Variables de classe
+
+//------------------------------------------------------------ Types privés
+
+
+//------------------------------------------------------------------ PUBLIC
+//--------------------------------------------------------- Fonctions amies
+
+//------------------------------------------------------ Méthodes publiques
+int Date::getHour () const
+{
+#ifdef MAP
+    cout << "Appel à la méthode getHour de <Date>" << endl;
+#endif
+    return hour;
+} //----- Fin de getHour
+
+
+//-------------------------------------------------- Surcharge d'opérateurs
+Date & Date::operator = ( const Date & aDate )
+// Algorithme :
+//
+{
+} //----- Fin de operator =
+
+ostream & operator << ( ostream & os, const Date & aDate)
+{
+    string zero = (day < 10) ? "0" : "";
+    os << '[' << zero << day;
+    os << '/' << month;
+    os << '/' << year;
+    zero = (hour < 10) ? "0" : "";
+    os << ':' << zero << hour;
+    zero = (minute < 10) ? "0" : "";
+    os << ':' << zero << minute;
+    zero = (second < 10) ? "0" : "";
+    os << ':' << zero << second;
+    char plus = (timeZone < 0) ? '-' : '+';
+    zero = (timeZone < 10) ? "0" : "";
+    os << ' ' << plus << zero << timeZone;
+    os << "00]";
+    return os;
+} //----- Fin de operator <<
+
+//--------------------------------------------- Constructeurs - destructeur
+Date::Date ( const Date & aDate )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de copie de <Date>" << endl;
+#endif
+} //----- Fin de Date (constructeur de copie)
+
+
+Date::Date ( 	int aHour,
+                int aMinute,
+                int aSecond,
+                int aTimeZone,
+                int aDay,
+                int aYear,
+                const string & aMonth
+            ) :
+            hour ( aHour ),
+            minute ( aMinute ),
+            second ( aSecond ),
+            timeZone ( aTimeZone ),
+            day ( aDay ),
+            year ( aYear )
+            month ( aMonth ),
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Date>" << endl;
+#endif
+} //----- Fin de Date
+
+
+Date::~Date ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Date>" << endl;
+#endif
+} //----- Fin de ~Date
+
+
+//------------------------------------------------------------------- PRIVE
+
+//------------------------------------------------------ Méthodes protégées
+
+//-------------------------------------------------------- Méthodes privées
